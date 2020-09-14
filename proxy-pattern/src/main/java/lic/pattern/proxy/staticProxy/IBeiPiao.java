ff551dbda854e13f072b79a40c061bd61d9e3e91
chengli <chengli@today36524.com.cn>
2020-09-14 20:14:37 +0800

    feat:代理模式

---
+++ proxy-pattern/src/main/java/lic/pattern/proxy/staticProxy/IBeiPiao.java
@@ -0,0 +1,13 @@
+package lic.pattern.proxy.staticProxy;
+
+/**
+ * @author ZerlindaLi create at 2020/9/14 20:13
+ * @version 1.0.0
+ * @description IBeiPiao
+ */
+public interface IBeiPiao {
+
+    public void buyTicket(){
+
+    }
+}
